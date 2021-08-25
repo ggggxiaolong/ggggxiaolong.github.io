bcb6e9f96935cf55663c77fb71e28f1485b8fae9
MrTan <freemrtan@gmail.com>
2021-08-25 11:16:33 +0800

    修复中文问题

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -53,6 +53,12 @@ module.exports = {
   plugins: [
     // "@vuepress/plugin-back-to-top",
     "@vuepress/plugin-medium-zoom",
-    // "@vuepress/blog",
+    [
+      'permalink-pinyin',
+      {
+        lowercase: true,
+        separator: '-'
+      }
+    ]
   ],
 };
